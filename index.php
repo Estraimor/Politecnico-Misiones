0f860fdc005f814ed0d7ad798552dca5cefa519d
Luciano Barros <lucianogastonbarros@gmail.com>
2024-03-31 17:46:01 -0300

    hola

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (empty($_SESSION["id"])){header('Location: ./login/login.php');}
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SGPM</title>
+    <title>hola</title>
     <link rel="stylesheet" type="text/css" href="./estilos.css">
     <link rel="stylesheet" type="text/css" href="./normalize.css">
     <link rel="icon" href="../politecnico.ico">
